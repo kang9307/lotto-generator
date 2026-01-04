23f222e6fd77924218bad29d3b5ddd22263926c8
Kang9307 <jhtoka@gmail.com>
2026-01-04 20:08:40 +0900

    Add multiple utility tools to static_index.html and update sitemap.xml: include new URLs for random menu, nickname generator, age calculator, character counter, and token counter with monthly change frequency and priority, enhancing user resources and engagement.

---
+++ static/token_counter_sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'token-counter-v1';
+const urlsToCache = [
+    '/static/token_counter.html',
+    '/static/token_counter_manifest.json'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch - Network first, fallback to cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
