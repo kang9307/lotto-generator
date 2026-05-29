ab6cca0e694570faafb07789f09a8f6b46118a3b
kang9307 <kang9307@gmail.com>
2026-05-30 08:23:48 +0900

    신규 포스팅(2026-05-30)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-05-29 발행: 신규 포스트 10편 × 4언어 = 40 URLs + blog.html + RSS × 4언어
+ * 2026-05-30 발행: 신규 포스트 8편 × 4언어 = 32 URLs + blog.html + RSS × 4언어
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,16 +9,14 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'developer_tools_2026_what_devs_actually_use',
-  'decepticon_red_team_ai_agent_2026',
-  'tired_of_talking_to_ai_2026',
-  'claude_opus_4_8_release_2026',
-  'tech_ceo_ai_psychosis_2026',
-  'duckduckgo_growth_ai_free_search_2026',
-  'codeboarding_ai_architecture_diagram_2026',
-  'naver_ai_citation_creator_compensation_2026',
-  'outsourcing_localai_cost_strategy_2026',
-  'anthropic_openai_product_market_fit_2026'
+  'orchestration_tax_multi_agent_2026',
+  'stackoverflow_decline_ai_coding_2026',
+  'ai_productivity_shorter_hours_2026',
+  'citly_ai_citation_tracker_2026',
+  'apple_google_notification_ai_editing_2026',
+  'sshc_rust_ssh_manager_2026',
+  'zirp_engineer_phenomenon_2026',
+  'why_gentoo_linux_philosophy_2026'
 ];
 
 const urls = [];
