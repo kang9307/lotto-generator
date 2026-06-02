f84bf6109ab6d07b5f3de15e866c80533b7d3c8c
kang9307 <kang9307@gmail.com>
2026-06-02 09:03:11 +0900

    신규 포스팅(2026-06-02)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-01 발행: 신규 포스트 8편 × 4언어 = 32 URLs + blog.html + RSS × 4언어
+ * 2026-06-02 발행: 신규 포스트 8편 × 4언어 = 32 URLs + blog.html + RSS × 4언어
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,14 +9,14 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'choose_boring_technology_ai_era_2026',
-  'anthropic_most_valuable_ai_startup_2026',
-  'is_mcp_dead_model_context_protocol_2026',
-  'claude_code_hidden_settings_2026',
-  'openrouter_series_b_multi_model_routing_2026',
-  'domain_expertise_real_moat_2026',
-  'human_value_argument_ai_era_obsolete_2026',
-  'microsoft_office_mac_perpetual_license_2026'
+  'engineering_in_the_llm_era_2026',
+  'files_sdk_unified_blob_storage_2026',
+  'ai_after_software_harness_era_2026',
+  'website_specification_writing_guide_2026',
+  'startup_survival_avoiding_death_2026',
+  'ai_agent_autonomy_safety_workaround_2026',
+  'vibe_coding_adhd_neurodiversity_2026',
+  'av2_video_codec_standard_2026'
 ];
 
 const urls = [];
