9f2c06df8ff2b3b553d9d7d90c6ccbe06e4dcd39
kang9307 <kang9307@gmail.com>
2026-06-07 10:14:52 +0900

    신규 포스팅(2026-06-07)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-06 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
+ * 2026-06-07 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,16 +9,16 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'ddr5_memory_price_surge_ai_demand_2026',
-  'anthropic_recursive_self_improvement_2026',
-  'voidzero_joins_cloudflare_vite_2026',
-  'ooooo_law_bloomberg_terminal_everyone_2026',
-  'building_ai_native_startups_2026',
-  'wind_solar_exceed_gas_2026',
-  'trees_file_tree_rendering_library_2026',
-  'spacex_ipo_retail_investor_criticism_2026',
-  'prediabetes_blood_sugar_control_guide',
-  'pension_irp_tax_saving_guide'
+  'korea_community_ai_image_censorship_2026',
+  'odysseus_self_hosting_ai_workspace_2026',
+  'code_economics_shifted_ai_2026',
+  'cpp_40_years_documentary_2026',
+  'govuk_stripe_to_adyen_payment_2026',
+  'solar_desalination_waste_free_2026',
+  'conventional_commits_critique_2026',
+  'ladybird_browser_development_changes_2026',
+  'tenant_rights_lease_protection_guide',
+  'bond_investing_beginner_guide'
 ];
 
 const urls = [];
