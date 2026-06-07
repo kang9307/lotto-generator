35b8a233bfbcb8e339d2612fa398af4e3782f7f7
kang9307 <kang9307@gmail.com>
2026-06-08 07:57:23 +0900

    신규 포스팅(2026-06-08)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-07 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
+ * 2026-06-08 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,16 +9,16 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'korea_community_ai_image_censorship_2026',
-  'odysseus_self_hosting_ai_workspace_2026',
-  'code_economics_shifted_ai_2026',
-  'cpp_40_years_documentary_2026',
-  'govuk_stripe_to_adyen_payment_2026',
-  'solar_desalination_waste_free_2026',
-  'conventional_commits_critique_2026',
-  'ladybird_browser_development_changes_2026',
-  'tenant_rights_lease_protection_guide',
-  'bond_investing_beginner_guide'
+  'google_spacex_compute_deal_2026',
+  'smart_tv_data_scraping_residential_proxy_2026',
+  'uuid_primary_key_sqlite_performance_2026',
+  'terminal_performance_developer_productivity_2026',
+  'nvidia_windows_unified_memory_pc_2026',
+  'kakaotalk_send_to_me_bookmark_2026',
+  'pokemon_emerald_webassembly_2026',
+  'openlogi_rust_logitech_alternative_2026',
+  'lower_blood_pressure_naturally_guide',
+  'reits_investing_guide'
 ];
 
 const urls = [];
