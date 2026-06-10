a56226fdc578bd88867cba29836e0a07ad51eacb
kang9307 <kang9307@gmail.com>
2026-06-10 09:06:53 +0900

    신규 포스팅(2026-06-10)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-09 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
+ * 2026-06-10 발행: hada 시사 8편 + 에버그린 2편 = 10편 × 4언어 = 40 URLs + blog.html × 4 + RSS × 4언어
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,16 +9,16 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'ai_backend_career_erosion_2026',
-  'kubernetes_gateway_api_nginx_ingress_2026',
-  'new_graduate_unemployment_rise_2026',
-  'linear_local_first_speed_2026',
-  'perry_typescript_llvm_compiler_2026',
-  'shortcat_macos_keyboard_control_2026',
-  'taste_as_the_new_10x_ai_era_2026',
-  'ioccc_2025_obfuscated_c_2026',
-  'labor_rights_wrongful_termination_guide',
-  'protein_supplement_guide'
+  'anthropic_fable5_mythos5_release_2026',
+  'apple_intelligence_siri_ai_2026',
+  'korea_gpki_firefox_root_certificate_2026',
+  'mq_jq_markdown_query_2026',
+  'ai_growth_slowdown_compute_2026',
+  'eu_banned_pesticide_food_residue_2026',
+  'vps_oom_google_deindex_seo_lesson_2026',
+  'antisocial_media_feed_trends_2026',
+  'thyroid_disorder_guide',
+  'writing_a_will_guide'
 ];
 
 const urls = [];
