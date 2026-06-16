70bdffdb29db3b7d1a22265f8155ed02e721d046
kang9307 <kang9307@gmail.com>
2026-06-17 08:39:55 +0900

    신규 포스팅(2026-06-17)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-16 발행: hada 10편 + 에버그린 2편 = 12편 × 4언어 = 48 URLs + blog × 4 + RSS × 4 = 56
+ * 2026-06-17 발행: hada 6편 + 에버그린 6편 = 12편 × 4언어 = 48 URLs + blog × 4 + RSS × 4 = 56
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,18 +9,18 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'local_llm_agentic_coding_2026',
-  'regent_ai_agent_version_control_2026',
-  'openai_codex_token_limit_reset_2026',
-  'claude_fable5_use_cases_2026',
-  'grit_git_rust_rewrite_2026',
-  'electronic_clam_clamshell_agent_2026',
-  'hera_agent_unity_mcp_2026',
-  'pm_prd_ai_drafting_2026',
-  'korea_gov_firefox_root_cert_2026',
-  'gukhanmun_hanja_hangul_converter_2026',
-  'metabolic_syndrome_guide',
-  'capital_gains_tax_guide'
+  'coreutils_for_windows_2026',
+  'homelab_ai_dev_platform_2026',
+  'apple_foundation_models_claude_2026',
+  'trump_mobile_htc_rebrand_2026',
+  'startup_support_search_service_2026',
+  'weekly_ai_ml_papers_2026_w24',
+  'atrial_fibrillation_guide',
+  'cataract_glaucoma_guide',
+  'property_tax_guide',
+  'industrial_accident_compensation_guide',
+  'school_violence_response_guide',
+  'homeowners_fire_insurance_guide'
 ];
 
 const urls = [];
