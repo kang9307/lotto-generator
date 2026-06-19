9eea3fab034e15c74e6495e0b9e555b4c2382f5d
kang9307 <kang9307@gmail.com>
2026-06-19 11:07:05 +0900

    신규 포스팅(2026-06-19)

--- _indexnow_submit.js
+++ _indexnow_submit.js
@@ -1,6 +1,6 @@
 /**
  * Bing IndexNow 배치 전송
- * 2026-06-18 발행: hada 3편 + 에버그린 9편 = 12편 × 4언어 = 48 URLs + blog × 4 + RSS × 4 = 56
+ * 2026-06-19 발행: hada 6편 + 에버그린 6편 = 12편 × 4언어 = 48 URLs + blog × 4 + RSS × 4 = 56
  */
 const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
@@ -9,27 +9,18 @@ const KEY = '7d594d096f044fbba3a09184f68ffcfe';
 const HOST = 'braindetox.kr';
 
 const NEW_POSTS = [
-  'ai_generated_ui_coherence_2026',
-  'codex_mobile_vibe_coding_2026',
-  'sandboxd_ai_app_builder_sandbox_2026',
-  'four_major_insurances_guide',
-  'menopause_management_guide',
-  'quit_smoking_guide',
-  'sleep_apnea_guide',
-  'index_fund_vs_etf_guide',
-  'dollar_investing_guide',
-  'home_buying_first_time_guide',
-  'car_buying_vs_leasing_guide',
-  'real_estate_vs_stocks_guide',
-  'spacex_cursor_acquisition_2026',
-  'local_token_router_gemma_2026',
-  'codeburn_ai_coding_cost_2026',
-  'claude_designer_ai_build_2026',
-  'agentic_code_review_2026',
-  'job_interview_kubernetes_2026',
-  'graph_ai_memory_mcp_2026',
-  'why_email_strangers_2026',
-  'john_carmack_bellard_2026'
+  'anthropic_seoul_office_2026',
+  'ktx_data_agent_context_2026',
+  'sqlite_hipp_turso_interview_2026',
+  'ai_organizational_automation_irony_2026',
+  'us_consumers_ai_branding_backlash_2026',
+  'cursor_origin_git_service_2026',
+  'jeonse_fraud_prevention_guide',
+  'car_accident_what_to_do_guide',
+  'personal_bankruptcy_rehabilitation_guide',
+  'debt_payoff_snowball_avalanche_guide',
+  'gold_investing_guide',
+  'ev_subsidy_guide'
 ];
 
 const urls = [];
